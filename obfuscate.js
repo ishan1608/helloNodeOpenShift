9c56a11c1dbda012b209ae8b3b229a7140ff644b
Ishan <ishanatmuzaffarpur@gmail.com>
2015-05-23 14:20:29 +0530

    CP code

---
+++ obfuscate.js
@@ -0,0 +1,11 @@
+var obfuscate = function(str){
+	var retString;
+	var len = str.length;
+    mstr = str + '';
+    str = mstr;
+	str1 = str.substr(0,len/2);
+	str2 = str.substr(len/2,len);
+	retString = str2+str1;
+	return retString;		
+};
+exports.obfuscate = obfuscate;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var http = require('http');
 var MongoClient = require('mongodb').MongoClient;
+var obfus = require('./obfuscate.js');
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-var mongoUri = 'mongodb://admin:z21ETW-caXWD@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/hellonode' || 'mongodb://127.0.0.1:27017/hellonode';
+var mongoUri = 'mongodb://admin:z21ETW-caXWD@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/hellonode';
+//var mongoUri = 'mongodb://127.0.0.1:27017/hellonode';
 
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -13,16 +15,42 @@ http.createServer(function(req, res) {
     MongoClient.connect(mongoUri, function(err, db) {
         if (!err) {
             res.write('\nConnected to mongoDB database\n\nMongoDB Info:\nHOST : ' + process.env.OPENSHIFT_MONGODB_DB_HOST + '\nPORT : ' + process.env.OPENSHIFT_MONGODB_DB_PORT);
-            var collection = db.collection('articles');
-            collection.findOne(function(err, result) {
-               if (!err) {
-                   res.end('\nFound item in collection articles :\n' + JSON.stringify(result));
-               } else {
-                   res.end('\nCouldn\'t find items in collection.');
-               }
+            // CP's section
+            user = new Object();
+            user.name = 'Ishan';
+            user.email = 'ishanatmuzaffarpur@gmail.com';
+            user.phonenumber = '+91-7696490368';
+            user.password = 'm7382in';
+            
+            var users = db.collection('users');
+            users.insert(user,{safe:true},function(err,result){
+                console.log('Result:');
+                console.dir(result);	// Works
+                console.log('Error::');
+                console.dir(err);
+                //JSON reply back!!
+                if(err){
+                    console.log('Something wrong happended: '+err);
+                    ob = new Object();
+                    ob.status = "1002";
+                    if(err.message.indexOf('E11000')!=-1)
+                        ob.message = '\nProblem - user already exists with this number';
+                    else
+                        ob.message = '\nsProblem - unknown error';
+                    res.end(JSON.stringify(ob));
+                }else{
+                    //Insert successful. return json object with data about user
+                    ob = new Object();
+                    ob.key = obfus.obfuscate(result[0]._id);	//  Not even result[0]._id.toString() or .toString
+                    ob.status = "1000";
+                    ob.message = '\nUser added to database successfully';
+                    res.end(JSON.stringify(ob));
+                }
+                db.close();
             });
+            var users = db.collection('users');
         } else {
-            res.end('Couldn\'t connect to mongoDB.\nError Info :\n' + JSON.stringify(err));
+            res.end('\nCouldn\'t connect to mongoDB.\nError Info :\n' + JSON.stringify(err));
         }
     });
 }).listen(port, ipaddress);
